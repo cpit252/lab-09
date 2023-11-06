4716fbce30f4dd768b6003cd00b3d050a7be5200
Khalid Alharbi <kalharbi@users.noreply.github.com>
2023-11-06 06:59:07 +0300

    Update Observer.java

--- src/main/java/edu/kau/fcit/cpit252/observers/Observer.java
+++ src/main/java/edu/kau/fcit/cpit252/observers/Observer.java
@@ -1,5 +1,15 @@
 package edu.kau.fcit.cpit252.observers;
 
 public abstract class Observer {
+    private String recipient;
 
+    public String getRecipient() {
+        
+    }
+
+    public void setRecipient(String recipient) {
+
+    }
+
+    public abstract void update(String m);
 }
